272eb23a10e842478e5da5abab89ef4a617b50f2
Adam Ridhwan <adamridhwan.1@gmail.com>
2023-12-14 20:57:21 -0500

    inital commit

--- advent_of_code_day_1/bin/advent_of_code_day_1.dart
+++ advent_of_code_day_1/bin/advent_of_code_day_1.dart
@@ -4,9 +4,32 @@ import 'package:path/path.dart' as p;
 
 void main(List<String> arguments) {
   var filePath = p.join(Directory.current.path, 'bin', 'text.txt');
+
   File file = File(filePath);
 
-  var fileContent = file.readAsStringSync();
+  var fileContent = file.readAsLinesSync();
 
-  print(fileContent);
+  int sum = 0;
+
+  var result = fileContent.map((string) {
+    String? firstDigit;
+    String? lastDigit;
+
+    for (var char in string.split('')) {
+      if (RegExp(r'\d').hasMatch(char)) {
+        lastDigit = char;
+        firstDigit ??= char;
+      }
+    }
+
+    return (firstDigit ?? '') + (lastDigit ?? '');
+  }).toList();
+
+  for (var numString in result) {
+    if (numString.isNotEmpty) {
+      sum += int.parse(numString);
+    }
+  }
+
+  print(sum);
 }
